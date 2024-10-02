cdd7fecf31af71c4911664fb13426da4980e9d62
myonur <onur@uludag.edu.tr>
2024-10-02 16:13:26 +0300

    ders 02

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 
+
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send('INDEX SAYFASI');
+    res.render('index', {
+        page_name : 'index'
+    });
 
 });
+
+
+app.get('/about', (req, res) => {
+    res.render('about' , {
+        page_name : 'about'
+    });
+
+});
+
 
 const port = 8000;
 app.listen(port, () => {
